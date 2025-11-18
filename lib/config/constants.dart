3d2b65853b86ad2cb8bda7262e0590d1890230e7
Rafi Shidqi <muhammadrafishidqi56@gmail.com>
2025-11-18 08:10:27 +0700

    tes

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,2 +1,2 @@
-const String baseUrl = "http://192.168.1.5:8000/api";     
+const String baseUrl = "http://192.168.137.1:8000/api";     
 // const String baseUrl = 'http://10.38.27.2:8000/api'; // IP asli
