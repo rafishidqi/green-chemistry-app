07e2ca31b9568b63374c2317cefe2513c258e81a
Hanz070731 <ikmalhanaanz31@gmail.com>
2025-11-18 08:07:43 +0700

    Ini yang baru

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,2 +1,2 @@
-const String baseUrl = "http://192.168.1.5:8000/api";     
+const String baseUrl = "http://10.103.210.19:8000/api";     
 // const String baseUrl = 'http://10.38.27.2:8000/api'; // IP asli
